0a05f3099dea85b15f8d7d07e31588a5f2179111
syuntoku14 <syuntoku14@keio.jp>
2019-04-02 10:20:24 -0700

    Fixed benchmark env

--- Flow_Benchmarks/common/utils.py
+++ Flow_Benchmarks/common/utils.py
@@ -6,6 +6,25 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
+from flow.scenarios.figure_eight import Figure8Scenario
+from flow.envs.loop.loop_accel import AccelEnv
+
+def make_vis_env(benchmark_name):
+    benchmark = __import__(
+        "flow.benchmarks.%s" % benchmark_name, fromlist=["flow_params"])
+    flow_params = benchmark.flow_params
+    env_params = flow_params['env']
+    sim_params = flow_params['sim']
+    sim_params.render = True
+    
+    scenario = Figure8Scenario(
+        flow_params['exp_tag'],
+        flow_params['veh'],
+        flow_params['net'],
+        initial_config=flow_params['initial'])
+    env = AccelEnv(env_params, sim_params, scenario)
+    
+    return env
 
 def plot_and_save(num_iters, rewards, image_path):
     clear_output(True)
