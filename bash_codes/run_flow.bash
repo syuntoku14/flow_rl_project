8f1ff84acfba9ebcdd25c4ff86a02be7ffab4637
syuntoku14 <syuntoku14@keio.jp>
2019-04-03 08:56:31 -0700

    Did ray_tutorial

--- bash_codes/run_flow.bash
+++ bash_codes/run_flow.bash
@@ -5,11 +5,12 @@ docker run --runtime=nvidia \
 -e NVIDIA_VISIBLE_DEVICES=all \
 -e DISPLAY=$DISPLAY \
 -v /home/syuntoku/rl_project:/headless/rl_project \
+-v /home/syuntoku/rl_project/ray_results:/headless/ray_results \
 -v /home/syuntoku/.Xauthority:/headless/.Xauthority:rw \
 -v /home/syuntoku/flow:/headless/flow \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 --entrypoint=/bin/bash \
 --shm-size 16G \
-syuntoku/flowtorch:ray_latest
+syuntoku/flowtorch:latest
 
 # lucasfischerberkeley/flowdesktop
