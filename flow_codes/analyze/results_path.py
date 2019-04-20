8c2c565160e98be095757dbb48011afd51281a27
syuntoku14 <syuntoku14@keio.jp>
2019-04-20 06:01:17 +0000

    Add DDPG (global optimum) result

--- flow_codes/analyze/results_path.py
+++ flow_codes/analyze/results_path.py
@@ -1,4 +1,4 @@
 # result file path
 
 PPO_PATH = 'PPO_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-18_22-06-061w6z291m'
-DDPG_PATH = 'DDPG_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-19_09-06-17ice5yjwu'
+DDPG_PATH = 'DDPG_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-20_05-09-10nau13ebn'
