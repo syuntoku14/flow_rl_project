f1f2b15e6d6d9cdd9ac1cd45c3353c729eb0febf
syuntoku14 <syuntoku14@keio.jp>
2019-04-19 19:25:37 -0700

    Delete failed DDPG results

---
+++ flow_codes/analyze/results_path.py
@@ -0,0 +1,4 @@
+# result file path
+
+PPO_PATH = 'PPO_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-18_22-06-061w6z291m'
+DDPG_PATH = 'DDPG_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-19_09-06-17ice5yjwu'
