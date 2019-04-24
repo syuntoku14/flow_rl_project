bc38af03a8d3bc469dc8858bf75bbe2b2088d881
syuntoku14 <syuntoku14@keio.jp>
2019-04-24 00:43:56 +0000

    Add PPO DDPG tune results

--- flow_codes/analyze/results_path.py
+++ flow_codes/analyze/results_path.py
@@ -1,4 +1,4 @@
 # result file path
 
-PPO_PATH = 'PPO_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-18_22-06-061w6z291m'
-DDPG_PATH = 'DDPG_MultiWaveAttenuationMergePOEnv-v0_0_2019-04-20_05-09-10nau13ebn'
+PPO_PATH = 'PPO_MultiWaveAttenuationMergePOEnv-v0_1_entropy_coeff=0.01,observation_filter=MeanStdFilter_2019-04-23_17-32-43oiiqbhe1'
+DDPG_PATH = 'DDPG_MultiWaveAttenuationMergePOEnv-v0_0_actor_hiddens=[64, 64],critic_hiddens=[64, 64],lr=0.001,observation_filter=MeanStdFilter_2019-04-23_06-08-35_b17350n'
